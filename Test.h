7dba562f8c7435b74396faba69188ffcff8af33c
Viswanth Chadalawada <viswanth.chadalawada@gmail.com>
2015-05-27 22:08:47 -0700

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Srikanth on 5/27/15.
+//  Copyright (c) 2015 ViswanthWorks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Srikanth on 5/27/15.
+//  Copyright (c) 2015 ViswanthWorks. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
